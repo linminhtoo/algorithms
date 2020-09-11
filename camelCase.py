eef5853d1fdfca06fa3e2bfc9926039107a0d536
Lin Min Htoo <linmin001@e.ntu.edu.sg>
2020-09-12 01:29:15 +0800

    more questions

---
+++ camelCase.py
@@ -0,0 +1,86 @@
+class Solution():
+    def camelMatch(self, qs, p):
+        def u(s):
+            return [c for c in s if c.isupper()]
+
+        def issup(s, t):
+            it = iter(t) # iter is quite hack
+            return all(c in it for c in s)
+        return [u(p) == u(q) and issup(p, q) for q in qs]
+
+class more_explainable_Solution():
+    def camelMatch(self, qs, p):
+        def u(s):
+            return [c for c in s if c.isupper()]
+
+        def issup(s, t):
+            for c in s:
+                if c in t:
+                    t = t[t.index(c)+1:]
+                else:
+                    return False
+            return True
+            
+        return [u(p) == u(q) and issup(p, q) for q in qs]
+
+class easy_Solution():
+    def camelMatch(self, inp: List[str], pat: str) -> List[bool]:
+        def pat_match(str, pat):
+            j = 0
+            for i in str:
+                if j < len(pat) and i == pat[j]:
+                    j += 1
+                elif ord('Z') >= ord(i) >= ord('A'):
+                    return False
+            return  j == len(pat) 
+
+        res = []
+        for i in inp:
+            res.append(pat_match(i, pat))
+        return res
+
+import collections
+from Typing import List
+# Trie, don't understand the helper function recursion properly
+class TrieNode:
+    def __init__(self):
+        self.child = collections.defaultdict(TrieNode)
+        self.is_word = False
+
+class Trie:
+    def __init__(self):
+        self.root = TrieNode()
+
+    def insert(self, word: str) -> None:
+        cur = self.root
+        for char in word:
+            cur = cur.child[char]
+        cur.is_word = True
+
+class Trie_Solution:
+    def camelMatch(self, queries: List[str], pattern: str) -> List[bool]:
+        def find(node, p_i, pattern, cur_word, table):
+            if p_i >= len(pattern):
+                if node.is_word:
+                    key = "".join(cur_word)
+                    table[key] = True
+                for k in node.child:
+                    if k.islower():
+                        find(node.child[k], p_i, pattern, cur_word+[k], table)
+            else:
+                for k in node.child:
+                    if k == pattern[p_i]:
+                        find(node.child[k], p_i+1, pattern, cur_word+[k], table)
+                    elif k.islower():
+                        find(node.child[k], p_i, pattern, cur_word+[k], table)
+        
+        trie = Trie()
+        for q in queries:
+            trie.insert(q)
+        
+        table = collections.defaultdict(lambda: False)
+        find(trie.root, 0, pattern, [], table)
+
+        return [table[q] for q in queries]
+
+# https://leetcode.com/problems/camelcase-matching/discuss/270029/JavaC%2B%2BPython-Check-Subsequence-and-Regax
---
+++ formingMagicSquare.py
@@ -0,0 +1,100 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# sample solution
+input_nums = []
+input_nums.extend([int(i) for i in input().split()])
+input_nums.extend([int(i) for i in input().split()])
+input_nums.extend([int(i) for i in input().split()])
+
+#input_nums[0], input_nums[1], input_nums[2] = [int(i) for i in input().split()]
+#input_nums[3], input_nums[4], input_nums[5] = [int(i) for i in input().split()]
+#input_nums[6], input_nums[7], input_nums[8] = [int(i) for i in input().split()]
+#print(input_nums)
+pos = []
+pos.append([8,1,6,3,5,7,4,9,2])
+pos.append([6,1,8,7,5,3,2,9,4])
+pos.append([4,9,2,3,5,7,8,1,6])
+pos.append([2,9,4,7,5,3,6,1,8])
+pos.append([8,3,4,1,5,9,6,7,2])
+pos.append([4,3,8,9,5,1,2,7,6])
+pos.append([6,7,2,1,5,9,8,3,4])
+pos.append([2,7,6,9,5,1,4,3,8])
+
+mindiff = 10000
+
+for arr in pos:
+    diff = 0
+    for i in range(9):
+        diff += abs(arr[i] - input_nums[i])
+    if (diff < mindiff):
+        mindiff = diff
+        
+print(mindiff)
+'''
+PROBLEM: the output matrices of rotateMultiple all end up becoming the same. no idea why
+'''
+
+# Complete the formingMagicSquare function below.
+def formingMagicSquare(s, N=3):
+    ''' 
+    N : int (Default = 3)
+        Size of the square 
+    '''
+    seed = [[6, 7, 2], [1, 5, 9], [8, 4, 3]]
+    magic_squares = []
+ 
+    for i in magic_squares:
+        print(i, end='\n')
+    # print('reflecting...')
+    # magic_squares = magic_squares + [reflectMatrix(matrix, N) 
+    #                                 for matrix in magic_squares]
+    
+    # for i in magic_squares:
+    #     print(i, end='\n')
+    
+
+def rotateMatrix(s, N=3):
+    for x in range(0, int(N/2)):
+        for y in range(x, N-x-1):
+            temp = s[x][y]
+            s[x][y] = s[N-1-y][x]
+            s[N-1-y][x] = s[N-1-x][N-y-1]
+            s[N-1-x][N-y-1] = s[y][N-1-x]
+            s[y][N-1-x] = temp
+    return s
+       
+       
+def rotateMultiple(s, N=3, K=1):
+    '''
+    K : int
+        How many times to rotate the matrix
+    '''
+    out = []
+    out.append(rotateMatrix(s))
+    if K == 1:
+        return out
+    out.append(rotateMatrix(rotateMatrix(s)))
+    if K == 2:
+        return out
+    out.append(rotateMatrix(rotateMatrix(rotateMatrix(s))))
+    if K == 3:
+        return out
+    out.append(rotateMatrix(rotateMatrix(rotateMatrix(rotateMatrix(s)))))
+    return out
+
+def reflectMatrix(s, N=3):
+    for x in range(0, N):
+        for y in range(0, int(N/2)):
+            temp = s[x][y]
+            s[x][y] = s[x][N-y-1]
+            s[x][N-y-1] = temp
+    return s 
+
+# formingMagicSquare(0)
+seed = [[6, 7, 2], [1, 5, 9], [8, 4, 3]]
