75c2019a8725116036b329400a7f8a38763d9ed0
Lin Min Htoo <linmin001@e.ntu.edu.sg>
2021-02-12 05:10:26 +0800

    Practice 3 tree traversal types

--- BinarySearchTree/medium/postorderTraversal.py
+++ BinarySearchTree/medium/postorderTraversal.py
@@ -7,6 +7,40 @@ class TreeNode:
         self.left = left
         self.right = right
 
+# https://leetcode.com/problems/binary-tree-postorder-traversal/discuss/45551/Preorder-Inorder-and-Postorder-Iteratively-Summarization
+# see comment from ofLucas
+# The reverse of pre-order is skillful though may not be general. 
+# Here I attach my solution of postorderTrav without reversing pre-order.
+# The key point is when you pop a node from stack, you ensure you have already explored its children.
+class Solution_iterative:
+    def postorderTraversal(self, root: TreeNode) -> List[int]:
+        if not root:
+            return []
+        
+        def isLeaf(node):
+            if not node: # only when we finished and want to break while curr loop
+                return True
+            return not node.left and not node.right
+            
+        res = []
+        stack = []
+        curr = root
+        while curr or stack:
+            while not isLeaf(curr):
+                stack.append(curr)
+                curr = curr.left
+            if curr:
+                res.append(curr.val)
+            while stack and curr == stack[-1].right:
+                curr = stack.pop() # what we popped is the parent of curr (which we explored already)
+                res.append(curr.val)
+            if not stack:
+                curr = None # want to break while curr loop
+            else:
+                curr = stack[-1].right
+        return res
+        
+
 class Solution_recursive:
     def postorderTraversal(self, root: TreeNode) -> List[int]:
         if not root:
@@ -17,3 +51,15 @@ class Solution_recursive:
         res.append(root.val)
         return res
 
+class Solution_recursive_helper:
+    def helper(self, root, res):
+        if not root:
+            return []
+        res = []
+        res.extend(self.helper(root.left, []))
+        res.extend(self.helper(root.right, []))
+        res.append(root.val)
+        return res
+        
+    def postorderTraversal(self, root: TreeNode) -> List[int]:
+        return self.helper(root, [])
